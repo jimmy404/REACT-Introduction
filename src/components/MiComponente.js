b8ddc35885dec3c0a902e932b4bc41d4714d90c4
Jimmy <bruno_pier@hotmail.com>
2019-12-26 15:00:52 -0300

    Adding map & key

--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 
 class MiComponente extends Component{
     render(){
+        let receta = {
+            nombre: 'Pizza',
+            ingredientes: ['Tomate', 'Queso', 'Jamon cocido'],
+            calorias: 400
+        }
+
         return(
-            <h1>Hola, soy el componente llamado: MiComponente</h1>
+            <React.Fragment>
+            <h1>{'Receta: ' + receta.nombre}</h1>
+            <h2>{'Calorias: ' + receta.calorias}</h2>
+            <ol>
+            {
+                receta.ingredientes.map((ingrediente, i) => {
+                    console.log(ingrediente);
+                    return(
+                        <li key={i}>
+                            {ingrediente}
+                        </li>
+                    )
+                })
+            }
+            </ol>
+            <hr/>
+            </React.Fragment>
         );
     }
 }
